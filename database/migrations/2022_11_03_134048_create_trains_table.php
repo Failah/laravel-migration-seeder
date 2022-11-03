0c9c1b89b7090bd7caca6ba44d5f3c3c4e13ecfc
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-03 14:57:52 +0100

    create table trains and migrated successfully

---
+++ database/migrations/2022_11_03_134048_create_trains_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    /*
+     Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+*/
+
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->mediumInteger('train_code')->unique();
+            $table->smallInteger('cabs_number');
+            $table->tinyInteger('in_time')->unsigned()->default(1);
+            $table->tinyInteger('deleted')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
